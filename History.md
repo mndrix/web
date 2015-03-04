50968d3279e95eef5fbe7cb59d099018fae95b17
Michael Hendricks <michael@ndrix.org>
2015-03-04 11:20:18 -0700

    Release v0.1.1

--- History.md
+++ History.md
@@ -1,3 +1,7 @@
+# v0.1.1 (2015-03-04)
+
+  * Repair packaging mistake
+
 # v0.1.0 (2015-03-04)
 
   * First public release
--- pack.pl
+++ pack.pl
@@ -1,11 +1,11 @@
 name(web).
 title('Easy web client').
 
-version('0.1.0').
-download('https://github.com/mndrix/web-prolog/archive/v0.1.0.zip').
+version('0.1.1').
+download('https://github.com/mndrix/web/archive/v0.1.1.zip').
 
 author('Michael Hendricks','michael@ndrix.org').
 packager('Michael Hendricks','michael@ndrix.org').
 maintainer('Michael Hendricks','michael@ndrix.org').
 
-home('https://github.com/mndrix/web-prolog').
+home('https://github.com/mndrix/web').
